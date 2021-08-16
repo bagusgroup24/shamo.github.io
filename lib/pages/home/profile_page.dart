b7a95626615747c7551feaa814f1b90ef893f7cd
Bagus Sanjaya Pratama <bagus.bip24@gmail.com>
2021-08-16 20:35:09 +0700

    complete main page

---
+++ lib/pages/home/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../theme.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Profil Page',
+        style: primaryTextStyle,
+      ),
+    );
+  }
+}
